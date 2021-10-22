d34d0e7d49e38c49303e4d9817ec31bbad900f82
Rachana Raut <rachanar@winjit.com>
2021-10-22 13:23:53 +0530

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {path:"",
+  component:HomeComponent
+},
+{path:"home",
+component:HomeComponent
+},
+{path:"login",
+component:LoginComponent
+},
+{path:"header",
+component:HeaderComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
